0fc8d41d2247d0db56337ed28030d0b15cb29bb8
KeremY96 <keremyavuzgames@gmail.com>
2023-10-28 13:15:43 +0200

    first commit

---
+++ modules.js
@@ -0,0 +1,7 @@
+const names = require('./names')
+const sayHi = require('./utils')
+
+
+
+
+sayHi(names.john)
---
+++ names.js
@@ -0,0 +1,6 @@
+const secret = 'Secret'
+
+const john = 'john'
+const peter = 'peter'
+
+module.exports = {john,peter}
